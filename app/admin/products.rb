bc218fbd11150fa1cff020aac760c7d54e39788f
Owen Beatty <owen.dj.beatty@gmail.com>
2020-04-02 18:56:20 -0500

    set up active admin

---
+++ app/admin/products.rb
@@ -0,0 +1,42 @@
+ActiveAdmin.register Product do
+  permit_params :name, :description, :price, :image, category_ids: []
+
+  index do
+    selectable_column
+    column :id
+    column :name
+    column :description
+    column :price
+    column :image
+    column :cateories do |product|
+      product.categories.map(&:name).join(", ").html_safe
+    end
+    actions
+  end
+
+  show do |product|
+    attributes_table do
+      row :name
+      row :description
+      row :price
+      row :image
+      row :cateories do |product|
+        product.categories.map(&:name).join(", ").html_safe
+      end
+    end
+    active_admin_comments
+  end
+
+  form do |f|
+    f.semantic_errors *f.object.errors.keys
+    f.inputs "Product" do
+      f.input :name
+      f.input :description
+      f.input :price
+      f.input :image
+      f.input :categories, as: :select, input_html: { multiple: true }
+    end
+
+    f.actions
+  end
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ApplicationRecord
+  has_and_belongs_to_many :categories
+  accepts_nested_attributes_for :categories
+
+  validate :has_one_or_more_categories
+
+  def has_one_or_more_categories
+    errors.add(:cateories, "- need at least one category") if categories.empty?
+  end
 end
