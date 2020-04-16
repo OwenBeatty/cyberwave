7befec7bee2194ff2fa9e0e43e63749f09e16f0e
Owen Beatty <owen.dj.beatty@gmail.com>
2020-04-16 01:18:43 -0500

    added visible cart and removing items

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: %i[show edit update destroy]
   before_action :initialize_session
+  before_action :load_cart
 
   # GET /products
   # GET /products.json
@@ -15,6 +16,12 @@ class ProductsController < ApplicationController
   def add_to_cart
     id = params[:id].to_i
     session[:cart] << id unless session[:cart].include?(id)
+    redirect_to root_path
+  end
+
+  def remove_from_cart
+    id = params[:id].to_i
+    session[:cart].delete(id)
     redirect_to root_path
   end
 
@@ -84,5 +91,9 @@ class ProductsController < ApplicationController
 
   def initialize_session
     session[:cart] ||= []
+  end
+
+  def load_cart
+    @cart = Product.find(session[:cart])
   end
 end
