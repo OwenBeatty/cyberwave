44f48f3f65772e36729688de5e0b24470d02c7b4
Owen Beatty <owen.dj.beatty@gmail.com>
2020-04-15 23:36:42 -0500

    set up user account creation/edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :set_categories
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
   private
 
   def set_categories
     @categories_nav = Category.all
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email password name address province])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[email password name address province province_id])
   end
 end
