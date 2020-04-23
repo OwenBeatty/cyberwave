d55322f493a4af29a9a630219a9d74ca69084de4
Owen Beatty <owen.dj.beatty@gmail.com>
2020-04-22 19:30:29 -0500

    added checkout button to cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_16_005709) do
+ActiveRecord::Schema.define(version: 2020_04_22_235223) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -70,6 +70,17 @@ ActiveRecord::Schema.define(version: 2020_04_16_005709) do
     t.integer "product_id", null: false
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "status"
+    t.string "address"
+    t.decimal "total"
+    t.string "note"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
   create_table "pages", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -112,5 +123,6 @@ ActiveRecord::Schema.define(version: 2020_04_16_005709) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "orders", "users"
   add_foreign_key "users", "provinces"
 end
