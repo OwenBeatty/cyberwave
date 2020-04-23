0cb671c8a5ee5d1dcec7cf17d56d3d6e86c37e5c
Owen Beatty <owen.dj.beatty@gmail.com>
2020-04-22 21:50:02 -0500

    finished order creation form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_23_012933) do
+ActiveRecord::Schema.define(version: 2020_04_23_023517) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 2020_04_23_012933) do
     t.text "note"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
