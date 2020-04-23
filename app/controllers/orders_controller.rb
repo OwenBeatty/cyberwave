89fa9dd3893b336f58164d5bb171ca397d92280f
Owen Beatty <owen.dj.beatty@gmail.com>
2020-04-22 20:27:49 -0500

    added autofill to user and address in order form

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,14 @@
 class OrdersController < InheritedResources::Base
+  before_action :initialize_session
+  before_action :load_cart
 
   private
 
-    def order_params
-      params.require(:order).permit(:user_id, :status, :address, :total, :note)
-    end
+  def order_params
+    params.require(:order).permit(:user_id, :status, :address, :total, :note)
+  end
 
+  def load_cart
+    @cart = Product.find(session[:cart])
+  end
 end
