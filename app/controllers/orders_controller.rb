475abbf1a522705793c3f738bb509a5ce40cad8d
Owen Beatty <owen.dj.beatty@gmail.com>
2020-04-22 21:34:19 -0500

    added order information and hidden fields

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,8 @@
 class OrdersController < InheritedResources::Base
   before_action :initialize_session
   before_action :load_cart
+  before_action :load_order_total
+  before_action :load_order_notes
 
   private
 
@@ -8,7 +10,28 @@ class OrdersController < InheritedResources::Base
     params.require(:order).permit(:user_id, :status, :address, :total, :note)
   end
 
+  def initialize_session
+    session[:cart] ||= []
+  end
+
   def load_cart
     @cart = Product.find(session[:cart])
+  end
+
+  def load_order_total
+    total = 0
+    Product.find(session[:cart]).each do |product|
+      total += product.price
+    end
+    @total = total
+    @taxes = (current_user.province.pst + current_user.province.gst + current_user.province.hst) * total
+  end
+
+  def load_order_notes
+    notes = ""
+    Product.find(session[:cart]).each do |product|
+      notes = notes + " " + product.name + " - $" + product.price.to_s + ", "
+    end
+    @note = notes
   end
 end
